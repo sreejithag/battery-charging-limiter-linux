3f4dd6294a5a0d0b77bcca260fe5ce804367839f
Sreejith <sreejith@localhost.localdomain>
2021-04-11 19:27:37 +0530

    added single script to enable service

---
+++ limitd.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+max="$1"
+
+if [ "$#" -ge  1 ]; then
+	if echo $max | egrep -q '^[0-9]+$'; then 
+		if [ "$max" -gt 100 ] || [ "$max" -le 0 ]; then
+			echo "Please eneter a valid max limit between [1-100]"
+		else
+			echo "Max battery capacity is limiting to $max % `tput setaf 2`✓ `tput sgr0`"
+			echo $max | sudo tee /sys/class/power_supply/BAT?/charge_control_end_threshold > /dev/null
+            cd /tmp
+            echo "[Unit]
+            Description=To set battery charge threshold
+            After=multi-user.target
+            StartLimitBurst=0
+
+            [Service]
+            Type=oneshot
+            Restart=on-failure
+            ExecStart=/bin/bash -c 'echo $max > /sys/class/power_supply/BAT0/charge_control_end_threshold'
+
+            [Install]
+            WantedBy=multi-user.target" > battery-manager.service
+
+            echo "created battery-manager.service `tput setaf 2`✓ `tput sgr0`"
+
+            sudo cp battery-manager.service /etc/systemd/system/
+
+            sudo systemctl enable battery-manager.service
+
+            echo "Battery manager service enabled `tput setaf 2`✓ `tput sgr0`"
+
+		fi
+	else
+		echo "Please enter a numeric max value"
+	fi
+else
+	echo "Please enter the max limit and try again"
+fi
