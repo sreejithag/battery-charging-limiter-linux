1eca46a484604f00e77cb1caefce6bf33f1b6a7c
Semih Aslan <110099203+andmydignity@users.noreply.github.com>
2023-05-21 13:27:56 +0300

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -5,7 +5,7 @@ max="$1"
 if [ "$#" -ge  1 ]; then
 	if echo $max | grep -E -q '^[0-9]+$'; then 
 		if [ "$max" -gt 100 ] || [ "$max" -le 0 ]; then
-			echo "Please eneter a valid max limit between [1-100]"
+			echo "Please enter a valid max limit between [1-100]"
 		else
 			echo "Max battery capacity is limiting to $1%"
 			echo $max | sudo tee /sys/class/power_supply/BAT?/charge_control_end_threshold			
